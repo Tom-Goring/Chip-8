ba496f1e12f79cac2bd6c2910e3e7bc3138819a0
Tom Goring <tgoring97@gmail.com>
2019-09-10 22:08:03 +0100

    Add Instruction enum

--- src/chip8.rs
+++ src/chip8.rs
@@ -4,15 +4,16 @@ const NUM_GENERAL_REGS: usize = 16;
 const NUM_KEYS: usize = 16;
 const DISPLAY_SIZE: usize = 64 * 32;
 
+
 pub struct Chip8 {
 	regs: [u8; NUM_GENERAL_REGS],
-	i_reg: u16,
-	sp: u8,
-	pc: u16,
-	memory: [u8; MEMORY_SIZE],
-	stack: [u16; NUM_STACK_FRAMES],
-	keyboard: [bool; NUM_KEYS],
-	display: [bool; DISPLAY_SIZE],
+	i_reg: u16, // address register
+	sp: u8, // stack pointer
+	pc: u16, // program counter
+	memory: [u8; MEMORY_SIZE], // memory storage
+	stack: [u16; NUM_STACK_FRAMES], // stack frames
+	keyboard: [bool; NUM_KEYS], // 16 keys
+	display: [bool; DISPLAY_SIZE], // 64 * 32 display
 	delay_timer: u8,
 	sound_timer: u8,
 }
@@ -32,5 +33,7 @@ impl Chip8 {
 			keyboard: [false; NUM_KEYS],
 			display: [false; DISPLAY_SIZE],
 		 }
+
+
 	}
 }
--- src/instruction.rs
+++ src/instruction.rs
@@ -1,3 +1,41 @@
 
 
-pub 
+pub enum Instruction {
+	SYS(addr),
+	ClearScreen,
+	Return,
+	Jump(addr),
+	Call(addr),
+	SkipIfEqualToByte(Register, u8),
+	SkipIfIfNotEqualToByte(Register, u8),
+	SkipIfRegisterEqualToRegister(Register, Register),
+	LoadByteToRegister(Register, u8),
+	AddByteToRegister(Register, u8),
+	LoadRegisterToRegister(Register, Register),
+	OR(Register, Register),
+	AND(Register, Register),
+	XOR(Register, Register),
+	ADD(Register, Register), // VF set to 1 if overflow
+	SUB(Register, Register), // VF set to 1 if negative overflow
+	SHR(Register), // Shift right 1 (binary divide by 2)
+	SUBN(Register, Register), // 2nd REG - 1st REG set VF if result negative
+	SHL(Register), // Shift left 1 (binary multiply by 2)
+	SNE(Register, Register), // Skip PC if not equal
+	LDI(addr), // Load i_reg with addr
+	JPV0(addr), // JMP to address + V0
+	RND(Register, u8), // Creates random number, AND's it with u8, then stores in Register
+	DRW(Register, Register, u8), // reads n bytes from i_reg, then displays them on screen at co-ords (Vx, Vy). 
+								 // Sprites are XOR'd onto the screen. Erased pixels cause VF t obe set to 1. 
+								 // Sprites wrap around to opposite sides of the screen.
+	SKP(Register), // Skips if key with value Register is pressed
+	SKNP(Register), // Skips if key with value Register is not pressed
+	LDDV(Register), // Value of delay timer is placed into Register
+	LDK(Register), // Wait for key press then store value of key into Register
+	LDVD(Register), // Set delay timer to value of Register
+	LDST(Register), // Set sound timer to value of Register
+	ADDI(Register), // Add Register to address register
+	LDS(Register), // Address register set to location for the sprite corresponding to Register
+	BCD(Register), // Store BCD representation of Register in memory locations I, I+1, I+2
+	SR, // Store Registers
+	LR // Load Registers
+}
