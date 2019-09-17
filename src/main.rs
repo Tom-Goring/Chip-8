b364b0422d995c12a19c719fedab2c958e73beab
Tom Goring <tgoring97@gmail.com>
2019-09-17 22:17:43 +0100

    Multiple additions/changes:
    Rework display to 2D array & confirm it works
    Write small test program to test sprites
    Finish rewriting register access

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 
+mod font;
 mod drivers;
 mod instruction;
 mod chip8;
@@ -18,10 +19,21 @@ const CHIP8_HEIGHT: usize = 32;
 const CHIP8_MEM: usize = 4096;
 
 fn main() {
-    let file_name = env::args().nth(1).expect("Expected a valid game name as argument!");
-	let mut file = File::open(file_name).expect("There was an issue opening the game file.");
+    // let file_name = env::args().nth(1).expect("Expected a valid game name as argument!");
+	// let mut file = File::open(file_name).expect("There was an issue opening the game file.");
 	let mut game_data = Vec::new();
-	file.read_to_end(&mut game_data).expect("Failure to read file.");
+	// file.read_to_end(&mut game_data).expect("Failure to read file.");
+
+    game_data.push(0xA2);
+    game_data.push(0x0A);
+    game_data.push(0x60);
+    game_data.push(0x0A);
+    game_data.push(0x61);
+    game_data.push(0x05);
+    game_data.push(0xD0);
+    game_data.push(0x17);
+    game_data.push(0x12);
+    game_data.push(0x08);
 
     let mut chip8 = chip8::Chip8::new(game_data);
     chip8.run();
