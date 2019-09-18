ab8631c99b6eef36e82466a1814d4f7638e59b6f
Tom Goring <tgoring97@gmail.com>
2019-09-18 19:05:32 +0100

    Multiple changes:
    Move back to rom testing
    Add a number of test roms to provoke failures
    Format font array to ensure it's correct
    Fix CALL and RET functions (hopefully)

--- src/main.rs
+++ src/main.rs
@@ -19,21 +19,10 @@ const CHIP8_HEIGHT: usize = 32;
 const CHIP8_MEM: usize = 4096;
 
 fn main() {
-    // let file_name = env::args().nth(1).expect("Expected a valid game name as argument!");
-	// let mut file = File::open(file_name).expect("There was an issue opening the game file.");
+    let file_name = env::args().nth(1).expect("Expected a valid game name as argument!");
+	let mut file = File::open(file_name).expect("There was an issue opening the game file.");
 	let mut game_data = Vec::new();
-	// file.read_to_end(&mut game_data).expect("Failure to read file.");
-
-    game_data.push(0xA2);
-    game_data.push(0x0A);
-    game_data.push(0x60);
-    game_data.push(0x0A);
-    game_data.push(0x61);
-    game_data.push(0x05);
-    game_data.push(0xD0);
-    game_data.push(0x17);
-    game_data.push(0x12);
-    game_data.push(0x08);
+	file.read_to_end(&mut game_data).expect("Failure to read file.");
 
     let mut chip8 = chip8::Chip8::new(game_data);
     chip8.run();
